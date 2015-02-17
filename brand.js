11f163a62d769569abc30478fee4b8c72b40a73b
Kevin Zweerink <kevin@kevinzweerink.com>
2015-02-17 10:08:07 -0500

    Create global color variables for js and scss files

---
+++ brand.js
@@ -0,0 +1,22 @@
+window.BRAND = {}
+
+BRAND.primaryColor = '#40C693';
+BRAND.interfaceColors = {
+	blue : '#83AFBA',
+	red : '#DB8D61',
+	yellow : '#B09E52'
+}
+BRAND.interfaceColorsDark = {
+	blue : '#63848C',
+	red : '#8F5C40',
+	yellow : '#87793F'
+}
+BRAND.interfaceColorsLight = {
+	blue : '#D3E6ED',
+	red : '#F2D8C7',
+	yellow : '#F2E7B8'
+}
+
+BRAND.grayScale = ['#1A2326','#404E54','#53646B','#799099','#9EBBC6','#D3E6ED']
+
+BRAND.dataScale = ['#9385B1','#91E789']
